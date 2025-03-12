167dd23d18d26636f256e16f653fa7464290dbca
mosgurekhyan <mos.gurekhyan@mail.ru>
2025-03-12 11:50:44 +0400

    forth_commit

--- src/constants/specializations.js
+++ src/constants/specializations.js
@@ -3,13 +3,13 @@ export const SPECIALIZATIONS = [
     id: '1',
     title: 'Frontend',
     text: 'I build unique and creative frontend experiences using React and modern web technologies',
-    projects: 14
+    projects: 30
   },
   {
     id: '2',
     title: 'Backend',
     text: 'I develop secure and scalable backend systems using Node.js',
-    projects: 5
+    projects: 25
   },
   {
     id: '3',
